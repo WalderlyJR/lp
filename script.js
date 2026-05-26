37bd281e563b686796fb76d099ae8c30110ee747
WalderlyJR <walderlyjunior123@gmail.com>
2026-05-26 19:19:27 -0300

    ajustes responsividade ajuste texto

--- script.js
+++ script.js
@@ -8,39 +8,34 @@ faqItems.forEach((item) => {
   });
 });
 
-const formCotacao = document.getElementById("formCotacao");
+/* banner de cookies */
 
-formCotacao.addEventListener("submit", function (event) {
-  event.preventDefault();
-
-  const mensagem = encodeURIComponent(
-    "Olá! Gostaria de receber uma cotação personalizada de plano de saúde."
-  );
-
-  window.open(`https://wa.me/5519998001107?text=${mensagem}`, "_blank");
-});
-
-/*banner de cookies*/
 const cookieBanner = document.getElementById("cookieBanner");
 
 const acceptCookies = document.getElementById("acceptCookies");
 
 const rejectCookies = document.getElementById("rejectCookies");
 
-if(localStorage.getItem("cookieChoice")){
-  cookieBanner.style.display = "none";
+if (cookieBanner && acceptCookies && rejectCookies) {
+
+  if (localStorage.getItem("cookieChoice")) {
+    cookieBanner.style.display = "none";
+  }
+
+  acceptCookies.addEventListener("click", () => {
+
+    localStorage.setItem("cookieChoice", "accepted");
+
+    cookieBanner.style.display = "none";
+
+  });
+
+  rejectCookies.addEventListener("click", () => {
+
+    localStorage.setItem("cookieChoice", "rejected");
+
+    cookieBanner.style.display = "none";
+
+  });
+
 }
-
-acceptCookies.addEventListener("click", () => {
-
-  localStorage.setItem("cookieChoice", "accepted");
-
-  cookieBanner.style.display = "none";
-});
-
-rejectCookies.addEventListener("click", () => {
-
-  localStorage.setItem("cookieChoice", "rejected");
-
-  cookieBanner.style.display = "none";
-});
